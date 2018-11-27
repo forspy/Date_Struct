84b9d850d394f68fed77a675c2107fe63d9d72a9
forspy <qiubow@126.com>
2018-11-27 15:25:07 +0800

    测试_c++DS_链式队列_注意弹出时的判断_进制转换小应用

--- test_c++DS_linked list Queue/test_c++DS_linked list Queue.cpp
+++ test_c++DS_linked list Queue/test_c++DS_linked list Queue.cpp
@@ -12,4 +12,14 @@ int main()
 	List.ShowAll();
 	cout << List.GetFirst() << endl;
 	cout << "队列的长度为：" << List.GetListLength() << endl;
+
+	int N = 10;
+	Queue List1;
+	while (N)
+	{
+		List1.Enqueue(N % 2);
+		N = N / 2;
+	}
+	cout << "十进制10转化位2进制，从右到左(右边为高位)" << endl;
+	List1.ShowAll();
 }
